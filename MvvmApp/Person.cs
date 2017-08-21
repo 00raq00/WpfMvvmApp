58f2f643e05d4ac39cef396bba0c9634f75ad0ae
00raq00 <00raq00@gmail.com>
2017-08-21 11:38:01 +0200

    implement INotifyPropertyChanged for Person.cs

--- MvvmApp/Person.cs
+++ MvvmApp/Person.cs
@@ -1,8 +1,49 @@
-﻿namespace MvvmApp
+﻿using System.ComponentModel;
+
+namespace MvvmApp
 {
-    public class Person
+    public class Person : INotifyPropertyChanged
     {
+        private string email;
+        
+        //if Name is changed UI doens't know about it
         public string Name { get; set; }
-        public string Email { get; set; }
+
+
+        //if Email is changed UI know about it and about changing FullName
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                if (email != value)
+                {
+                    email = value;
+                    RaisePropertyChanged("Email");
+                    RaisePropertyChanged("FullName");
+                }
+            }
+        }
+
+        public string FullName
+        {
+            get
+            {
+                return Name + " --> email: " + Email;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
     }
 }
